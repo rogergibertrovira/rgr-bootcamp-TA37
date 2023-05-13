8d4c5a5fe01bb8e46d8edcd4c08c6c48e7026230
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-13 14:39:08 +0200

    Start

---
+++ rickandmorty/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { CharacterCardComponent } from './character-card/character-card.component';
+import { CharactersComponent } from './characters/characters.component';
+import { CharactersGroupComponent } from './characters-group/characters-group.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    CharacterCardComponent,
+    CharactersComponent,
+    CharactersGroupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ rickandmorty/src/app/characters-group/characters-group.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-group',
+  templateUrl: './characters-group.component.html',
+  styleUrls: ['./characters-group.component.css']
+})
+export class CharactersGroupComponent {
+
+}
